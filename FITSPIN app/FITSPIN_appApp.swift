df0d47ec6a66b4bcf7461901aefaf824a604e9db
aksiniapomi <kseniapominova@gmail.com>
2025-04-04 16:48:57 +0100

    Initial Xcode project: FITSPIN app

---
+++ FITSPIN app/FITSPIN_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  FITSPIN_appApp.swift
+//  FITSPIN app
+//
+//  Created by Xenia Uni Account on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FITSPIN_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
