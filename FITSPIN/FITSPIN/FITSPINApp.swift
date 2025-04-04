a8b59ee18f0a23e8a976c6155bba85ae7260dea9
aksiniapomi <kseniapominova@gmail.com>
2025-04-04 14:04:00 +0100

    Initial Xcode project setup

---
+++ FITSPIN/FITSPIN/FITSPINApp.swift
@@ -0,0 +1,17 @@
+//
+//  FITSPINApp.swift
+//  FITSPIN
+//
+//  Created by Xenia Uni Account on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FITSPINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FITSPIN/FITSPINTests/FITSPINTests.swift
@@ -0,0 +1,16 @@
+//
+//  FITSPINTests.swift
+//  FITSPINTests
+//
+//  Created by Xenia Uni Account on 04/04/2025.
+//
+
+import Testing
+
+struct FITSPINTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
