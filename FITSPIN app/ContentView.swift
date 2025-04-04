f79fbda01aa41b3018b24ee7032791e59fdc648d
aksiniapomi <kseniapominova@gmail.com>
2025-04-04 16:51:28 +0100

    Updated text on welcome screen

--- FITSPIN app/ContentView.swift
+++ FITSPIN app/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "clock")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, FITSPIN!")
         }
         .padding()
     }
